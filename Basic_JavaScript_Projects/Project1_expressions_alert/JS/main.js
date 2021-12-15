3030e4370a7425eea2b5a270e5b9b8252115cdda
ruizd2218 <ruizd2218@gmail.com>
2021-12-15 14:03:05 -0800

    finished assignments.

--- Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,5 @@
-document.write("Hello, World!");
+var A ="25"; //setting of var A
+var B ="12"; //setting of var B
+var C = A.fontcolor("blue");//setting the color of var C to A with a blue font color.
+document.write(C); //printing B on the website
+window.alert(A + B); //printing the concatenation of var A and var B.
