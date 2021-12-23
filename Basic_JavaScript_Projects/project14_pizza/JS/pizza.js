323c9d83e482da614d030d59924f1cc5a6665c20
ruizd2218 <ruizd2218@gmail.com>
2021-12-23 14:40:55 -0800

    assignment

--- Basic_JavaScript_Projects/project14_pizza/JS/pizza.js
+++ Basic_JavaScript_Projects/project14_pizza/JS/pizza.js
@@ -2,11 +2,11 @@ function getReceipt() {
     //this initializes our string so it can get passed from
     //function to function, growing line by line into a full receipt
     var text1 = "<h3>You Ordered:</h3>"
-    var runningTotal = 0;
+    var runningTotal = 0; 
     var sizeTotal = 0;
-    var sizeArray = document.getElementsByClassName("size");
-    for (var i = 0; i < sizeArray.length; i++) {
-        if (sizeArray[i].checked)
+    var sizeArray = document.getElementsByClassName("size"); 
+    for (var i = 0; i < sizeArray.length; i++) { //for loop for variable i. if i is less than sizeArray.length then we continue running the loop and increment i by 1 each time
+        if (sizeArray[i].checked) {
             var selectedSize = sizeArray[i].value;
             text1 = text1+selectedSize+"<br>";
         }
@@ -22,6 +22,7 @@ function getReceipt() {
     } else if (selectedSize === "Extra Large Pizza") {
         sizeTotal = 16;
     }
+    //runningtotal becomes the total of the pizza size
     runningTotal = sizeTotal;
     console.log(selectedSize+" = $"+sizeTotal+".00");
     console.log("size text1: "+text1);
@@ -29,19 +30,23 @@ function getReceipt() {
     //these variables will get passed on to each function
     getTopping(runningTotal,text1);
 
+    //function to add toppings to the pizza
     function getTopping(runningtTotal, text1) {
+        //variable to keep track of topping price
         var toppingTotal=0;
+        //array to keep track of toppings
         var selectedTopping = [];
         var toppingArray = document.getElementsByClassName("toppings");
+        //for loop for variable j = 0. if j is less than toppingArray, then we can keep incrementing it by 1.
         for (var j = 0; j < toppingArray.length; j++) {
             if (toppingArray[j].checked) {
-                selectedTopping.push(toppingArray[j].value);
+                selectedTopping.push(toppingArray[j].value); //this adds toppings to the Array.
                 console.log("selected topping item: ("+toppingArray[j].value+")");
-                text1 = text1+toppingArray[j].value+"<br>";
+                text1 = text1+toppingArray[j].value+"<br>"; 
             }
         }
-        var toppingCount = selectedTopping.length;
-        if(toppingCount > 1) {
+        var toppingCount = selectedTopping.length; //the value of topping count becomes the length selectedTopping
+        if(toppingCount > 1) { //if the number is greater than one, it -1, because of the deal that we have
             toppingTotal = (toppingCount - 1);
         } else {
             toppingTotal = 0;
@@ -51,8 +56,8 @@ function getReceipt() {
         console.log(toppingCount+" topping - 1 free topping = "+"$"+toppingTotal+".00");
         console.log("topping text1: "+text1);
         console.log("Purchase Total: "+"$"+runningTotal+".00");
-        document.getElementById("showText").innerHTML=text1;
-        document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$"+
+        document.getElementById("showText").innerHTML=text1; //showText now is edited to be text1
+        document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$"+//this now shows the total price in an <h3> element below
             runningTotal+".00"+"</strong></h3>";
-    };
-
+    }
+}
